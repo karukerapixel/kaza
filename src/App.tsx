975a4687463068cd96daaab28aa0a9b1870b7af1
karukerapixel <karukerapixel@gmail.com>
2025-01-15 15:27:38 +0100

    define new route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Housing from './pages/Housing';
+import Favorite from 'pages/Favorite';
 import NotFoundPage from './pages/NotFoundPage';
 
 const App: React.FC = () => {
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/housings/:id" element={<Housing />} />
+        <Route path="/fav-housings/" element={<Favorite />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Route>
     </Routes>
